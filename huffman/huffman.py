aa53df31ff98f9755281547079deb62c784c4651
Uche Okereke <orezpablo@gmail.com>
2023-04-29 01:28:46 +0200

    refactor huffman algorithm

--- huffman/huffman.py
+++ huffman/huffman.py
@@ -1,36 +1,27 @@
 import collections
-import heapq
-from heap.heap import Heap, Pair
 from dataclasses import dataclass, field
 from typing import Any
+
+from heap.heap import Heap, Pair
 
 
 @dataclass(order=True)
 class TreeNode:
     sort_index: int = field(init=False, repr=False)
-    # element: str
     chars: str
     freq: int
-    # priority: int
     left: Any = None
     right: Any = None
 
     def __post_init__(self):
         self.sort_index = self.freq
-        # self.sort_index = self.priority
 
 
 def huffman(text):
     # char_frequencie_map = compute_frequencies(text)
     # priority_queue = []
 
-    # freq_to_heap(compute_frequencies(text), priority_queue)
-
-    # heap_to_tree(freq_to_heap(compute_frequencies(text), priority_queue))
-
-    return build_table(
-        heap_to_tree(freq_to_heap(compute_frequencies(text))), "", {}
-    )
+    return build_table(heap_to_tree(freq_to_heap(compute_frequencies(text))), "", {})
 
 
 def build_table(node, sequence, chars_to_sequence_map):
@@ -53,25 +44,18 @@ def freq_to_heap(freq_map):
     heap = []
     for ch, freq in freq_map.items():
         heap.append(Pair(TreeNode(ch, freq), freq))
-        # heapq.heappush(heap, TreeNode(ch, freq))
 
     return Heap(heap)
-    # return heap
 
 
 def heap_to_tree(heap):
     while len(heap) > 1:
         right = heap.top()
-        # right = heapq.heappop(heap)
         left = heap.top()
-        # left = heapq.heappop(heap)
 
         parent = TreeNode(left.chars + right.chars, left.freq + right.freq)
-        # parent = TreeNode(left.element + right.element, left.priority + right.priority)
         parent.left = left
         parent.right = right
-        # heapq.heappush(heap, parent)
         heap.insert(parent, parent.freq)
 
-    # return heapq.heappop(heap)
     return heap.top()
