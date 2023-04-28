c5e89ca0ad207ed73c23156f327bd951bc15a947
Uche Okereke <orezpablo@gmail.com>
2023-04-29 01:25:18 +0200

    refactor heap class

--- heap/heap.py
+++ heap/heap.py
@@ -9,7 +9,9 @@ class Pair:
 
 
 class Heap:
-    def __init__(self, pairs=[], branches=2):
+    def __init__(self, pairs=None, branches=2):
+        if pairs is None:
+            pairs = []
         self.pairs = pairs
         self.branches = branches
         # self.hash_map = {}
@@ -44,13 +46,13 @@ class Heap:
         else:
             element = self.pairs[0].element
             self.pairs[0] = p
-            self._push_down(self.pairs)
+            self._push_down()
             return element
 
     def insert(self, element, priority):
         p = Pair(element, priority)
         self.pairs.append(p)
-        self._bubble_up(self.pairs, len(self.pairs) - 1)
+        self._bubble_up(len(self.pairs) - 1)
 
     def remove(self, element):
         index = self.tuple_index(element, self.pairs)
@@ -60,9 +62,9 @@ class Heap:
         parent_index = self._get_parent_index(index, self.branches)
 
         if self.pairs[parent_index].priority > self.pairs[index].priority:
-            self._bubble_up(self.pairs, index)
+            self._bubble_up(index)
         elif self.pairs[parent_index].priority < self.pairs[index].priority:
-            self._push_down(self.pairs, index)
+            self._push_down(index)
 
     def update(self, element, new_priority):
         index = self.tuple_index(element, self.pairs)
@@ -70,34 +72,34 @@ class Heap:
             old_priority = self.pairs[index].priority
             self.pairs[index] = Pair(element, new_priority)
             if old_priority > new_priority:
-                self._bubble_up(self.pairs, index)
+                self._bubble_up(index)
             elif old_priority < new_priority:
-                self._push_down(self.pairs, index)
+                self._push_down(index)
 
-    def _bubble_up(self, pairs, index):
-        current = pairs[index]
+    def _bubble_up(self, index):
+        current = self.pairs[index]
         while index > 0:
             parent_index = self._get_parent_index(index)
-            if pairs[parent_index].priority > current.priority:
-                pairs[index] = pairs[parent_index]
+            if self.pairs[parent_index].priority > current.priority:
+                self.pairs[index] = self.pairs[parent_index]
                 # self.hash_map[pairs[index].element] = index
                 index = parent_index
             else:
                 break
-        pairs[index] = current
+        self.pairs[index] = current
         # self.hash_map[pairs[index].element] = index
 
-    def _push_down(self, pairs, index=0):
-        current = pairs[index]
-        while index < self._first_leaf_index(pairs):
+    def _push_down(self, index=0):
+        current = self.pairs[index]
+        while index < self._first_leaf_index():
             (child, child_index) = self._highest_priority_child(index)
             if child.priority < current.priority:
-                pairs[index] = pairs[child_index]
+                self.pairs[index] = self.pairs[child_index]
                 # self.hash_map[pairs[index].element] = index
                 index = child_index
             else:
                 break
-        pairs[index] = current
+        self.pairs[index] = current
         # self.hash_map[pairs[index].element] = index
 
     # def contains(self, elem):
@@ -114,8 +116,8 @@ class Heap:
     def _get_parent_index(self, index):
         return int((index - 1) / self.branches)
 
-    def _first_leaf_index(self, pairs):
-        return (len(pairs) - 2) // self.branches + 1
+    def _first_leaf_index(self):
+        return (len(self.pairs) - 2) // self.branches + 1
 
     def _highest_priority_child(self, index):
         child_indx = []
@@ -133,8 +135,22 @@ class Heap:
         return self.pairs[highest_priority], highest_priority
 
     def _heapify(self):
-        last_inner_node_index = self._first_leaf_index(self.pairs) - 1
+        last_inner_node_index = self._first_leaf_index() - 1
         for index in range(last_inner_node_index, -1, -1):
-            self._push_down(self.pairs, index)
+            self._push_down(index)
 
-    
+    def _validate(self):
+        current_index = 0
+        first_leaf = self._first_leaf_index()
+        while current_index < first_leaf:
+            current_priority = self.pairs[current_index].priority
+            first_child = self._first_child_index(current_index)
+            last_child_guard = min(first_child + self.branches, len(self))
+            for child_index in range(first_child, last_child_guard):
+                if current_priority > self.pairs[child_index].priority:
+                    return False
+            current_index += 1
+        return True
+
+    def _first_child_index(self, index):
+        return index * self.branches + 1
