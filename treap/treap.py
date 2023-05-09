9cf2b67ea1ad1667a211f1110217212961317594
Uche Okereke <orezpablo@gmail.com>
2023-05-09 16:25:09 +0200

    treap insert

--- treap/treap.py
+++ treap/treap.py
@@ -29,11 +29,45 @@ class Node:
     def is_left_child(self):
         return
 
-    def search(self, key, priority):
-        pass
+    def search(self, key):
+        if self is None:
+            return None
+        if self.key == key:
+            return self
+        elif key < self.key:
+            return self.left.search(key)
+        else:
+            return self.right.search(key)
 
-    def add(self, key, priority):
-        pass
+    def add(self, treap, key, priority):
+        node = treap.root
+        parent = None
+        new_node = Node(key, priority)
+
+        while node is not None:
+            parent = node
+            if node.key <= key:
+                node = node.left
+            else:
+                node = node.right
+        if parent is None:
+            treap.root = new_node
+            return
+        elif key <= parent.key:
+            parent.left = new_node
+        else:
+            parent.right = new_node
+        new_node.parent = parent
+
+        while (
+            new_node.parent is not None and new_node.priority < new_node.parent.priority
+        ):
+            if new_node == new_node.parent.left:
+                new_node._right_rotate()
+            else:
+                new_node._left_rotate()
+        if new_node.parent is None:
+            treap.root = new_node
 
     def _left_rotate(self):
         if self.is_root():
